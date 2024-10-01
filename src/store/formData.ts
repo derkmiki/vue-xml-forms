22cc7686e70f0d1ca94b1b0853ee28074122c806
Edward Florindo <derk.miki.2023@gmail.com>
2024-10-01 19:18:51 +0800

    20240110: Major change to use xml as data input, us xml-js for bidirectional conversion and updating the uischema to work with the new json schema

--- src/store/formData.ts
+++ src/store/formData.ts
@@ -1,18 +1,19 @@
-import { defineStore } from "pinia";
-import { computed, ref } from "vue";
-import intialData from '../json/data.json';
-import { toXML } from 'jstoxml';
-import xmlFormat from 'xml-formatter';
+import { defineStore } from "pinia"
+import { computed, ref } from "vue"
+import initialData from '../data/data.xml?raw'
+import convert from 'xml-js'
+import xmlFormat from 'xml-formatter'
 
 export const useFormDataStore = defineStore('formData', () => {
-    const data = ref<Object>(intialData)
+    const data = ref<Object>(convert.xml2js(initialData, { 'compact': true, 'ignoreDeclaration': true, alwaysArray: true }))
     const setData = (newData: Object) => {
+        console.log(JSON.stringify(newData));
         data.value = newData
     }
     const xmlData = computed(() => {
-        return xmlFormat('<root>' + toXML(data.value) + '</root>')
+        return xmlFormat(convert.js2xml(data.value, { 'compact': true, 'ignoreDeclaration': true }))
     });
 
-    return {data, setData, xmlData}
+    return { data, setData, xmlData }
 
 })
